a5c14b8050fcacc27deeb2bda45f983b53e56b66
Diego Villacís <diegovillacis101@gmail.com>
2016-10-11 11:47:08 -0500

    @jreyes33 [KFE-204] Fix compile errors and restore ViewController.swift

---
+++ Example/kushki-ios/ViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+import kushki_ios
+
+class ViewController: UIViewController {
+
+
+    // MARK: Properties
+    @IBOutlet weak var doItHttpButton: UIButton!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var monthField: UITextField!
+    @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var cvvField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: Actions
+
+    @IBAction func requestToken(_ sender: UIButton) {
+        let publicMerchantId = "10000001641125237535111218"
+        let card = Card(name: nameField.text!, number: numberField.text!, cvv: cvvField.text!, expiryMonth: monthField.text!, expiryYear: yearField.text!)
+        let totalAmount = 10.00
+        let kushki = Kushki(publicMerchantId: publicMerchantId,
+                            currency: "USD",
+                            environment: KushkiEnvironment.testing)
+        kushki.requestToken(card: card, totalAmount: totalAmount) { transaction in
+            let message = transaction.token != "" ? transaction.token : (transaction.code + ": " + transaction.text)
+            DispatchQueue.main.async(execute: {
+                let alert = UIAlertController(title: "Aurus Token",
+                                              message: message,
+                                              preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default))
+                self.present(alert, animated: true)
+            })
+        }
+    }
+
+}
