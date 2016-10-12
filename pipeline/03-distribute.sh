e046ceadaff57ad6d1c8131f0c4fe3fcc1d31cd6
Jonathan Reyes <j@jreyes.org>
2016-10-11 19:19:47 -0500

    [KFE-204] Remove unnecessary (and erronous) extra checks on deployment
    - Pull requests are never deployed:
    https://docs.travis-ci.com/user/deployment/#Pull-Requests
    
    - We are only building the master branch (see .travis.yml file)

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -2,16 +2,6 @@
 
 set -eo pipefail
 
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  echo 'This is a pull request. Skipping deployment.'
-  exit 0
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" != 'master' ]; then
-  echo "This is branch $TRAVIS_BRANCH, not master. Skipping deployment."
-  exit 0
-fi
-
 artifact_version=$(grep "version.*=" kushki-ios.podspec | cut -d "'" -f 2)
 tag_name="v$artifact_version"
 found_tag=$(git tag | grep "^$tag_name$" || true)
