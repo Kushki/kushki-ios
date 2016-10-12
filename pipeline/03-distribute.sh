d044c1886368fab84790d4340f8f8c25390fdbf8
Jonathan Reyes <j@jreyes.org>
2016-10-11 22:40:22 -0500

    [KFE-204] Switch back the Ruby version for deployment in CI

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+# Travis CI switches back to an old Ruby version for deployments. See:
+# https://docs.travis-ci.com/user/deployment/script/#Deployment-is-executed-by-Ruby-1.9.3
+rvm use default
 echo "cocoapods gem version: $(pod --version)"
 artifact_version=$(grep "version.*=" kushki-ios.podspec | cut -d "'" -f 2)
 tag_name="v$artifact_version"
