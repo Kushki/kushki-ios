b88dd24f78baee4e9385e6d28e6203e18d022e5e
Jonathan Reyes <j@jreyes.org>
2017-01-04 14:29:38 -0500

    [KFE-239] Fix bug with travis

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
   -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.0' -sdk iphonesimulator10.0 \
-  ONLY_ACTIVE_ARCH=NO | xcpretty
+  ONLY_ACTIVE_ARCH=NO | xcpretty -tc
