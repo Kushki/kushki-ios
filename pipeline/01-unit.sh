9ec06e4d73c40cfb9589c479253441e8b1b2657e
Juan Erazo <juan@kushkipagos.com>
2018-11-12 12:54:03 -0500

    [KV-3485] update ios version

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
-xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
-  -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.0' -sdk iphonesimulator10.0 \
-  ONLY_ACTIVE_ARCH=NO | xcpretty -tc
+xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example
+  -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.1' -sdk iphonesimulator \
+  ONLY_ACTIVE_ARCH=NO | xcpretty -c 
