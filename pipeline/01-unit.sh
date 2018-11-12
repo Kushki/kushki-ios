a189f126f42d4d6434f9d9d4edf46a4a180027c2
Juan Erazo <juan@kushkipagos.com>
2018-11-12 15:24:02 -0500

    [KV-3485] fix script

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
-xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example
+xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
   -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.1' -sdk iphonesimulator \
-  ONLY_ACTIVE_ARCH=NO | xcpretty -c 
+  ONLY_ACTIVE_ARCH=NO -allowProvisioningUpdates
