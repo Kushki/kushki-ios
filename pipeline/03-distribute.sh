b62cc6c78e0288e742e38c3606a65ccab694f47b
moransk8 <davidm@kushkipagos.com>
2019-03-20 09:55:50 -0500

    deploy

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -6,23 +6,19 @@ set -eo pipefail
 # https://docs.travis-ci.com/user/deployment/script/#Deployment-is-executed-by-Ruby-1.9.3
 echo "start distribute"
 # source ~/.rvm/scripts/rvm
-echo "print source"
 rvm use default
-echo "print rvm use default"
 echo "cocoapods gem version: $(pod --version)"
-echo "version"
 artifact_version=$(grep "version.*=" Kushki.podspec | cut -d "'" -f 2)
 tag_name="v$artifact_version"
 found_tag=$(git tag | grep "^$tag_name$" || true)
 
-echo "print artifact version"
 if [ ! -z "$found_tag" ]; then
   echo "Version $artifact_version already exists. Skipping deployment."
   exit 0
 fi
 
-git config user.email jose@kushkipagos.com
-git config user.name 'José Santacruz'
+git config user.email dev@kushkipagos.com
+git config user.name 'Kushki'
 git remote add tags-origin "https://$GITHUB_TOKEN@github.com/Kushki/kushki-ios.git"
 git tag --annotate "$tag_name" -m "Release for version $artifact_version"
 git push tags-origin "$tag_name"	
