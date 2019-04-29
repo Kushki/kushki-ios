f9a43d0207ffe2e8851b44802e95927b628789f4
juan-ee <juanque@outlook.com>
2019-04-29 11:29:46 -0500

    [KV-4026] Migrate to swift 5

--- Example/Pods/Kushki/Kushki/Classes/Kushki.swift
+++ Example/Pods/Kushki/Kushki/Classes/Kushki.swift
@@ -4,23 +4,24 @@ public class Kushki {
     
     private let publicMerchantId: String
     private let currency: String
-    private let aurusClient: AurusClient
+    private let kushkiClient: KushkiClient
 
-    public convenience init(publicMerchantId: String, currency: String, environment: KushkiEnvironment) {
-        self.init(publicMerchantId: publicMerchantId, currency: currency, environment: environment, aurusEncryption: AurusEncryption())
-    }
-
-    // Constructor for testing
-    init(publicMerchantId: String, currency: String, environment: KushkiEnvironment, aurusEncryption: AurusEncryption) {
+    public init(publicMerchantId: String, currency: String, environment: KushkiEnvironment, regional: Bool = false) {
         self.publicMerchantId = publicMerchantId
         self.currency = currency
-        self.aurusClient = AurusClient(environment: environment, aurusEncryption: aurusEncryption)
+        self.kushkiClient = KushkiClient(environment: environment, regional: regional)
     }
     
     public func requestToken(card: Card,
                              totalAmount: Double,
                              completion: @escaping (Transaction) -> ()) {
-        let requestMessage = aurusClient.buildParameters(withMerchantId: self.publicMerchantId, withCard: card, withAmount: totalAmount)
-        self.aurusClient.post(endpoint: "/tokens", requestMessage: requestMessage, withCompletion: completion)
+        let requestMessage = kushkiClient.buildParameters( withCard: card, withCurrency: self.currency, withAmount: totalAmount)
+        self.kushkiClient.post(withMerchantId: self.publicMerchantId, endpoint: "/tokens", requestMessage: requestMessage, withCompletion: completion)
+    }
+    
+    public func requestSubscriptionToken(card: Card,
+                                         completion: @escaping (Transaction)->()){
+        let requestMessage = kushkiClient.buildParameters(withCard: card, withCurrency: self.currency)
+        self.kushkiClient.post(withMerchantId: self.publicMerchantId, endpoint: "/subscription-tokens", requestMessage: requestMessage, withCompletion: completion)
     }
 }
