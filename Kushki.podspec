259c405ece4be46db8ee166b38d92938720e32fc
Kevin Segura <kfe_seg93@hotmail.com>
2021-01-29 17:30:11 -0500

    [11712] Change version to beta

--- Kushki.podspec
+++ Kushki.podspec
@@ -3,10 +3,10 @@ Pod::Spec.new do |s|
   s.version = '2.2.2-beta'
   s.summary = 'Kushki iOS library.'
   s.description = 'iOS library to integrate with Kushki.'
-  s.homepage = 'https://bitbucket.org/Kushki/kushki-ios'
+  s.homepage = 'https://github.com/Kushki/kushki-ios'
   s.license = { :type => 'MIT', :file => 'LICENSE' }
   s.author = { 'Kushki' => 'soporte@kushkipagos.com' }
-  s.source = { :git => 'https://bitbucket.org/Kushki/kushki-ios.git', :tag => "v#{s.version.to_s}" }
+  s.source = { :git => 'https://github.com/Kushki/kushki-ios.git', :tag => "v#{s.version.to_s}" }
   s.ios.deployment_target = '12.1'
   s.source_files = 'Kushki/Classes/**/*'
 end
--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -8,18 +8,18 @@ echo "start distribute"
 # source ~/.rvm/scripts/rvm
 rvm use default
 echo "cocoapods gem version: $(pod --version)"
-echo "BITBACKEND: $BITBUCKET"
 artifact_version=$(grep "version.*=" Kushki.podspec | cut -d "'" -f 2)
 tag_name="v$artifact_version"
 found_tag=$(git tag | grep "^$tag_name$" || true)
+
 if [ ! -z "$found_tag" ]; then
   echo "Version $artifact_version already exists. Skipping deployment."
   exit 0
 fi
 
-git config user.email "seguridad@kushkipagos"
-git config user.name "segKushki"
-git remote add tags-origin "https://segKushki@bitbucket.org/kushki/kushki-ios.git"
+git config user.email dev@kushkipagos.com
+git config user.name 'Kushki'
+git remote add tags-origin "https://$GITHUB_TOKEN@github.com/Kushki/kushki-ios.git"
 git tag --annotate "$tag_name" -m "Release for version $artifact_version"
 git push tags-origin "$tag_name"	
 pod trunk push
