fc2345f21891c00bd3975027670c4441801bdc97
Kevin Segura <kfe_seg93@hotmail.com>
2021-02-15 01:04:21 -0500

    [ISD-0002] Modify distribution file

--- pipeline/03-distribute.sh
+++ pipeline/03-distribute.sh
@@ -6,21 +6,18 @@ set -eo pipefail
 # https://docs.travis-ci.com/user/deployment/script/#Deployment-is-executed-by-Ruby-1.9.3
 echo "start distribute"
 # source ~/.rvm/scripts/rvm
-rvm use default
+# rvm use default
 echo "cocoapods gem version: $(pod --version)"
-echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 artifact_version=$(grep "version.*=" Kushki.podspec | cut -d "'" -f 2)
 tag_name="v$artifact_version"
 found_tag=$(git tag | grep "^$tag_name$" || true)
+echo "tag_name: $tag_name"
+echo "found_tag: $found_tag"
+echo "artifact_version: $artifact_version"
 
 if [ ! -z "$found_tag" ]; then
   echo "Version $artifact_version already exists. Skipping deployment."
   exit 0
 fi
-
-git config user.email dev@kushkipagos.com
-git config user.name 'Kushki'
-git remote add tags-origin "https://$GITHUB_TOKEN@github.com/Kushki/kushki-ios.git"
-git tag --annotate "$tag_name" -m "Release for version $artifact_version"
-git push tags-origin "$tag_name"	
+	
 pod trunk push
