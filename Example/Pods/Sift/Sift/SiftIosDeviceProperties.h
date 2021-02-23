13ce936daa7fcf41c266980160963d10919c2d0c
iraidamercedes <iraida.barreto@kushkipagos.com>
2021-02-23 15:06:26 -0500

    [FP-1386] Sift Sicence en iOS

--- Example/Pods/Sift/Sift/SiftIosDeviceProperties.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016 Sift Science. All rights reserved.
-
-@import Foundation;
-
-#import "SiftHtDictionary.h"
-
-SiftHtDictionary *SFMakeEmptyIosDeviceProperties(void);
-
-SiftHtDictionary *SFCollectIosDeviceProperties(void);
--- Example/Pods/Target Support Files/Sift/Sift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Sift : NSObject
-@end
-@implementation PodsDummy_Sift
-@end
