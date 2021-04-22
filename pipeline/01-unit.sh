82c7a8615f9ea84a06f9b74be3c2740a0f0b36db
edwar95 <davidmoralesp1995@gmail.com>
2021-04-22 18:21:19 -0500

    merged with github master

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
-  -destination 'platform=iOS Simulator,name=iPhone 8,OS=14.3' -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 8,OS=14.4' -sdk iphonesimulator \
   ONLY_ACTIVE_ARCH=NO -allowProvisioningUpdates
