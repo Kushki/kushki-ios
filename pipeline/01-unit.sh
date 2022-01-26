a04458ac3263f5859beb242f1a765431dfe3c14d
Yesica.Nava <yesica.nava@MacBook-Pro-de-invitado.local>
2022-01-26 09:44:03 -0500

    deploy devops librerios ios

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -eo pipefail
 xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
   -destination 'platform=iOS Simulator,name=iPhone 12 Pro Max,OS=15.0' -sdk iphonesimulator \
