d51212f4e71601c20be96fdc757fca016db2b691
Yesica.Nava <yesica.nava@MacBook-Pro-de-invitado.local>
2022-05-25 16:55:06 -0500

    deploy libreria ios bin 2.3.1

--- pipeline/01-unit.sh
+++ pipeline/01-unit.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 xcodebuild test -workspace Example/kushki-ios.xcworkspace -scheme kushki-ios-Example \
   -destination 'platform=iOS Simulator,name=iPhone 12 Pro Max,OS=15.5' -sdk iphonesimulator \
-  ONLY_ACTIVE_ARCH=NO -allowProvisioningUpdates
+  ONLY_ACTIVE_ARCH=YES -allowProvisioningUpdates
